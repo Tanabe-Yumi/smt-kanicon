5938265d48474fe4e6366746cdeabc15e8e6071f
Tanabe-Yumi <alleyoop58dank@gmail.com>
2022-12-08 16:02:30 +0900

    two blocks from above can be inverse transformed

--- src/lib/ruby-to-blocks-converter/kani-blocks.js
+++ src/lib/ruby-to-blocks-converter/kani-blocks.js
@@ -6,34 +6,50 @@ import _ from 'lodash';
 */
 const KaniBlocksConverter = {
 // eslint-disable-next-line no-unused-vars
-	onSend: function (receiver, name, args, rubyBlockArgs, rubyBlock) {
+	onSend: function (receiver, name, args, rubyBlockArgs, rubyBlock, variable) {
 		console.log("in onsend - kani");
 		console.log("name: " + name);
 		console.log("args: ");
 		console.dir(args);
 		console.log("receiver: ");
 		console.dir(receiver);
-		//console.log("rubyblockargs: " + rubyBlockArgs);
-		//console.log("rubyblock: " + rubyBlock);
+		// console.log("rubyblockargs: " + rubyBlockArgs);
+		// console.log("rubyblock: " + rubyBlock);
+		console.dir(variable);
 		
 		let block;
 		//if ((this._isSelf(receiver) || receiver === Opal.nil) && !rubyBlock) {
-		//console.log("_isSelf(receiver): " + this._isSelf(receiver));
-		//console.log("receiver === Opal.nil: " + receiver === Opal.nil);
 		if (this._isSelf(receiver) || receiver === Opal.nil) {
-			console.log("!rubyBlock in onSend");
-			console.log("name: " + name);
+			// console.log("!rubyBlock in onSend");
 			switch (name) {
 			case 'motorEn':
 				console.log("in motorEn");
-				if(args.length === 0){
+				// if(args.length === 0){}
+				break;
+			default:
+				console.log("in default");
+				break;
+			}
+		} else {
+			console.log("in else - onsend");
+			switch (name) {
+			case 'on':
+			case 'off':
+				console.log("in case on/off");
+				const pat = /motorEn/;
+				if(args.length === 0 && pat.test(variable.name)){
 					console.log("in args.length = 0");
-					block = this._createBlock('kani_motor_enable_set_n', 'shape_statement');
-					console.log("before add");
-					console.dir(block);
-					//this._addField(block, 'enable', 'on');
-					this._addTextInput(block, 'enable', 'on', 'on');
-					console.log("after add");
+					console.dir(receiver.fields);
+					block = this._createBlock('kani_motor_enable_set_n', 'statement', {
+						fields: {
+							enable: {
+								name: 'enable',
+								id: variable.id,
+								value: name,
+								variableType: '',
+							}
+						}
+					});
 					console.dir(block);
 				}
 				break;
@@ -61,7 +77,7 @@ const KaniBlocksConverter = {
 		//console.log("name: " + name);
 		
 		let block;
-		//console.log("variable.name: " + variable.name);
+		console.log("variable.name: " + variable.name);
 		switch (variable.name) {
 		case 'motorEn':
 			//console.log("in motorEn");
@@ -69,8 +85,7 @@ const KaniBlocksConverter = {
 			//console.log("code: " + code);
 			//console.log("pat.test: " + pat.test(code));
 			if(pat.test(code)){
-				//console.log("in args.length = 0");
-				block = this._createBlock('kani_motor_enable_init_n', 'shape_statement');
+				block = this._createBlock('kani_motor_enable_init_n', 'statement');
 			}
 			break;
 		default:
